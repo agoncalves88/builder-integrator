abf50140fc22158c1f57cd54a6bd4c315aea89f9
anderson alves <andrsonalves@gmail.com>
2022-11-01 15:54:14 -0300

    implementacao de go routines para processamento em paralelo

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -2,13 +2,15 @@ package configuration
 
 type Config struct {
 	TESTE       string
-	DATASOURCES []struct {
-		NAME              string
-		URL               string
-		HAS_PARAM         bool
-		PROPERTIES_TO_GET []struct {
-			VALUE_TO_GET string
-			INPUT_NAME   string
-		}
+	DATASOURCES []DataSource
+}
+
+type DataSource struct {
+	NAME              string
+	URL               string
+	HAS_PARAM         bool
+	PROPERTIES_TO_GET []struct {
+		VALUE_TO_GET string
+		INPUT_NAME   string
 	}
 }
--- service/builderService.go
+++ service/builderService.go
@@ -2,10 +2,10 @@ package service
 
 import (
 	"builder-integrator/configuration"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync"
 
 	"github.com/tidwall/gjson"
 )
@@ -15,37 +15,40 @@ type BuilderService struct {
 }
 
 func (service BuilderService) GetDynamicServices(param string) map[string]interface{} {
+	var waitGroup sync.WaitGroup
+
+	waitGroup.Add(len(service.Config.DATASOURCES))
 	inputToBuild := map[string]interface{}{
 		"CORRELATION": "",
 	}
 
 	for _, datasource := range service.Config.DATASOURCES {
-		var ret *http.Response
-		var err error
-		if datasource.HAS_PARAM {
-			ret, err = http.Get(fmt.Sprintf(datasource.URL, param))
-			checkApiErr(err)
-		} else {
-			ret, err = http.Get(datasource.URL)
-			checkApiErr(err)
-		}
-
-		body, _ := ioutil.ReadAll(ret.Body)
-		fmt.Println(string(body))
-
-		json_map := make(map[string]interface{})
-		_ = json.Unmarshal(body, &json_map)
-
-		for _, prop := range datasource.PROPERTIES_TO_GET {
-			inputToBuild[prop.INPUT_NAME] = gjson.Get(string(body), prop.VALUE_TO_GET).String()
-		}
-
+		go getService(datasource, param, &waitGroup, &inputToBuild)
 	}
 
-	// jsonBuilded, _ := json.Marshal(inputToBuild)
-	// fmt.Println(string(jsonBuilded))
-
+	waitGroup.Wait()
 	return inputToBuild
+}
+
+func getService(datasource configuration.DataSource, param string, group *sync.WaitGroup, inputBuilded *map[string]interface{}) {
+	var ret *http.Response
+	var err error
+	if datasource.HAS_PARAM {
+		ret, err = http.Get(fmt.Sprintf(datasource.URL, param))
+		checkApiErr(err)
+	} else {
+		ret, err = http.Get(datasource.URL)
+		checkApiErr(err)
+	}
+
+	body, _ := ioutil.ReadAll(ret.Body)
+	toBuilded := *inputBuilded
+	for _, prop := range datasource.PROPERTIES_TO_GET {
+		toBuilded[prop.INPUT_NAME] = gjson.Get(string(body), prop.VALUE_TO_GET).String()
+	}
+	*inputBuilded = toBuilded
+
+	group.Done()
 }
 func checkApiErr(err error) {
 	if err != nil {
